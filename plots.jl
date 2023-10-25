e1825dbc51c7ade7f93182cd4f8719ee61b1dd0f
Daniel Sinderson <drsinderson@gmail.com>
2023-10-25 07:50:02 -0700

    Finished rough draft of section introducting symmetries in a simple algebraic context

--- plots.jl
+++ plots.jl
@@ -7,8 +7,10 @@ x = [i for i in -2:0.1:2]
 y1 = [i for i in x]
 y2 = [i+1 for i in x]
 
-scatter!(x, y1, marker = '|', markersize = 30)
-scatter!(x, y1)
-scatter!(x, y2)
+scatter!(x, y1, markersize=15, label="untransformed")
+lines!(x, y1, linewidth=5, label="symmetric transformation")
+lines!(x, y2, linewidth=5, label="nonsymmetric transformation")
+
+axislegend()
 
 f
